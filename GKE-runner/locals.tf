abdcf82aabb21227e543d56706c06394bab2b4dc
daphney Igwe <daphney.igwe@deimos.co.za>
2022-05-12 10:32:52 +0100

    removed unnecessary values from the locals.tf file

--- GKE-runner/locals.tf
+++ GKE-runner/locals.tf
@@ -7,11 +7,9 @@ locals {
     "roles/monitoring.viewer"
   ], var.additional_node_service_account_roles))
 
-  # runner_name       = var.runner_name != null ? var.runner_name : "runner-${random_id.random_suffix.hex}"
   cred_file         = "/secrets/gcs_cred"
   count             = var.cache_type == "gcs" ? 1 : 0
   cache_secret_name = "google-application-credentials"
-  # cache_bucket_name = var.cache_bucket_name != null ? var.cache_bucket_name : "${var.prefix}-cache-${random_id.random_suffix.hex}"
 
 
   gcs_config = {
@@ -19,5 +17,4 @@ locals {
     BucketName      = "${module.cache.0.cache_bucket_name}"
   }
   gcs = var.cache_type == "gcs" ? local.gcs_config : {}
-  # release = var.runner_release_name != null ? var.runner_release_name : "runner-${random_id.random_suffix.hex}"
 }
