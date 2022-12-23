17d1c3f29eedce7a31c3a3db3a288a622c3227d1
yanw2 <wenjing.yan@kasna.com.au>
2022-12-23 12:33:00 +1100

    Fix a bug

--- modules/cache/local.tf
+++ modules/cache/local.tf
@@ -1,3 +1,3 @@
 locals {
-  bucket_name = var.bucket_name != null ? var.bucket_name : "${var.prefix}-cache-${random_id.suffix[0].hex}"
+  bucket_name = var.bucket_name != null ? var.bucket_name : "${var.prefix}-cache-${random_id.suffix.hex}"
 }
